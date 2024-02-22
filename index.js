78df6d923a8da2c714437787651d949e82b9661c
Aubrey Whitford <Aubreya.whitford@gmail.com>
2024-02-22 17:49:29 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+  }
+
+  function destructivelyPrependCat() {
+    cats.unshift("Bob")
+  }
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo")
+}
+
+function appendCat(name) {
+    let newCats = [...cats]
+    newCats.push(name)
+    return newCats
+}
+
+function prependCat(name) {
+    let newCats = [...cats]
+    newCats.unshift(name)
+    return newCats
+}
+
+function removeLastCat() {
+    const newCats = cats.slice(0, -1)
+    newCats.shift
+    return newCats
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(1)
+    newCats.shift
+    return newCats
+}
+
